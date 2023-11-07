a065a0ff59183277a06bf0fea58a3c697cec0cc7
minhtuan1407-telnyx <tuan@telnyx.com>
2023-11-07 10:52:52 +0700

    change: update code session 8

---
+++ 8. arithmetic operators/HelloWorld.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // arithmetic operators = perform arithmetic on variables and constants
+
+    // + = addition
+    // - = subtraction
+    // * = multiplication
+    // / = division
+    // % = modulus (remainder)
+    // ++ = increment
+    // -- = decrement
+
+    int x = 5;
+    int y = 2;
+
+    x++; // x = x + 1
+    y--; // y = y - 1
+
+    printf("Calculation of %d and %d\n", x, y);
+
+}
