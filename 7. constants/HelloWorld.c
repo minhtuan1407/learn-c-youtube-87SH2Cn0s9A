6d2d7f698bf2a267efaeaf0edd1389a2c1b574f1
minhtuan1407-telnyx <tuan@telnyx.com>
2023-11-07 10:47:08 +0700

    change: update code session 7

---
+++ 7. constants/HelloWorld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // constant = fixed value that cannot be changed by the program during normal execution
+
+    const float PI = 3.14159;
+
+    printf("The value of pi is %f\n", PI);
+
+    return 0;
+}
