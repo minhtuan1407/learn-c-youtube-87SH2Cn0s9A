91902388fe8454cd95770a9d949647417c0a665a
minhtuan1407 <m.tuan9a2@gmail.com>
2023-11-12 15:10:07 +0700

    change: update code session 32

---
+++ 32. arrays/HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    // arrays = a data structure that can store many values of the same type
+
+    double prices[10] = {1.2, 2.3, 3.4, 4.5, 5.6};
+
+    printf("%.2lf\n", prices[0]);
+
+    return 0;
+}
